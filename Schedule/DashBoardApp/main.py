fd4c4640f4c05e803cb3468d941458a3f1455a96
Arran Hemish <arran.hemish@hitachivantara.com>
2019-01-29 15:40:07 +0000

    fix appointment update and delete

--- Schedule/DashBoardApp/main.py
+++ Schedule/DashBoardApp/main.py
@@ -11,50 +11,52 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config.from_pyfile('config.cfg')
 
-
-db = sqlite3.connect('sqlite/schedule.db')
-db_session = SQLAlchemy(app)
+db = SQLAlchemy(app)
 from models import *
 
 
-cursor = db.cursor()
 
 def getAppoinments():
     
-    df = pd.read_sql('SELECT * FROM appointments', db)
-    appointmentsJson = df.to_json(orient='records')
-    appointments = json.loads(appointmentsJson)
-    
+    all_appointments = [{
+        "id" : i[0].id,
+        "start" : i[0].start,
+        "end" : i[0].end,
+        "allDay" : i[0].all_day,
+        "className" : i[0].appointment_colour,
+        "patient_id" : i[0].patient_id,
+        "appointment_type" : i[0].appointment_type,
+        "cancelled" : i[0].canceled,
+        "title" : i[1].name,
+        "email" : i[1].email,
+        "phoneNumber" : i[1].phone_number,
+        "notes" : i[1].notes
+    } for i in db.session.query(Appointment,Patient).join(Patient, Appointment.patient_id == Patient.id).all()]
 
-    for i in appointments:
-        if i["appointment_colour"] != None:
-            i["appointment_colour"] = i.get("appointment_colour", "").split(" ")
-    return appointments
-
-
-    
-    
-
+    return all_appointments
 
 
 @app.route('/autocomplete', methods=['GET'])
 def autocomplete():
     search = request.args.get('q')
-    #query = db_session.session.query(Appointment.title).filter(Appointment.title.like('%' + str(search) + '%'))
-    #results = [p[0] for p in query.all()]
-    return jsonify({'d':'d'})#jsonify(matching_results=results)
+    query = db.session.query(Patient.name).filter(Patient.name.like('%' + str(search) + '%'))
+    results = [p[0] for p in query.all()]
+    return jsonify(matching_results=results)
 
 
 
 @app.route('/getPatient', methods=['POST'])
 def getPatient():
-    
-    patientDf = pd.read_sql('SELECT * FROM appointments where title = (?) ', db , params=(request.form["data"],))
-    
-    patientAppointmentJson = patientDf.to_json(orient='records')
-    patientAppointment = json.loads(patientAppointmentJson)
-    
-    return jsonify(patientAppointment)
+
+    patient_name = request.form["data"]
+    patient_appointment = [{
+        "id" : i.id,
+        "title" : i.name,
+        "email" : i.email,
+        "phoneNumber" : i.phone_number,
+        "notes" : i.notes
+    } for i in Patient.query.filter_by(name=patient_name).all()]
+    return jsonify(patient_appointment)
 
 
 @app.route('/addAppointment', methods=['POST'])
@@ -72,11 +74,11 @@ def addAppointment():
     canceled=data['canceled']
     
     #Does Patient already exist add if not
-    patient_id = db_session.session.query(Patient.id).filter_by(name=patient_name).scalar()
+    patient_id = db.session.query(Patient.id).filter_by(name=patient_name).scalar()
     if patient_id is  None:
         new_patient = Patient(name=patient_name,email=patient_email,phone_number=patient_phone_number,notes=patient_notes)
-        db_session.session.add(new_patient)
-        db_session.session.commit()
+        db.session.add(new_patient)
+        db.session.commit()
         patient_id = Patient.query.filter_by(name=patient_name).first().id
 
     new_appointment = Appointment(
@@ -88,45 +90,55 @@ def addAppointment():
         appointment_type=appointment_type,
         canceled =canceled
     )
-    db_session.session.add(new_appointment)
-    db_session.session.commit()
+    db.session.add(new_appointment)
+    db.session.commit()
 
-        
-    
-    return ""
+    return jsonify({'id' : new_appointment.id})
     
 
 
 @app.route('/updateAppointment', methods=['POST'])
 def updateAppointment():
     
-    appointment = json.loads(request.data)
-    appointments = db_session.session.query(Appointment,Patient).join(Appointment.patient_id == Patient.id ).all()
+    data = json.loads(request.data)
+    appointment_id = data['id']
+    patient_name=data['patientName']
+    patient_phone_number=data['phoneNumber']
+    patient_notes=data['notes']
+    start=data['start']
+    end=data['end']
+    all_day=data['allDay']
+    appointment_colour=data['appointmentColour']
+    patient_email=data['email']
+    appointment_type=data['appointmentType']
+    canceled=data['canceled']
 
-    #appointment['className'] = "".join(appointment.get("className"))
-    cursor.execute(''' DELETE FROM appointments WHERE id = ? and title = ? ''',(appointment.get("id"), appointment.get("oldTitle", appointment.get("title"))))
-    db.commit()
-    df = pd.DataFrame(appointment,index=[0])
-    if(appointment.get("oldTitle") != None):
-        
-        df = df.drop(["oldTitle"],axis=1)
-    df.to_sql('appointments', con=db, if_exists='append', index=False)
+    appointment = Appointment.query.filter_by(id=appointment_id).first()
+    appointment.start = start
+    appointment.end = end
+    appointment.all_day = all_day
+    appointment.appointment_colour = appointment_colour
+    appointment.appointment_type = appointment_type
+    appointment.canceled = canceled
     
-    
-    
-    
-    return ""
+    patient = Patient.query.filter_by(id=appointment.patient_id).first()
+    patient.name = patient_name
+    patient.email = patient_email
+    patient.phone_number = patient_phone_number
+    patient.notes = patient_notes
+    db.session.commit()
+    return jsonify({'status' : 'Success' })
 
     
 
 @app.route('/deleteAppointment', methods=['POST'])
 def deleteAppointment():
-    req = json.loads(request.data)
-    cursor.execute(''' DELETE FROM appointments WHERE id = ? and title = ? ''',(req.get("id"), req.get("title")))
-    db.commit()
-    
-    print(req.get("id"), req.get("title"))
-    return ""
+    data = json.loads(request.data)
+    id = data["id"] 
+    appointment = Appointment.query.filter_by(id=id).delete()
+    db.session.commit()
+
+    return jsonify({'status' : 'Success'})
 
 @app.route('/')
 def homepage():
@@ -154,10 +166,7 @@ def overview():
 
 @app.route('/calendar')
 def calendar():
-
-    
 	event= getAppoinments()
-	print(event)
 	return render_template('dashboard/pages/AJAX_Full_Version/calendar.html' , event=event)
 
 
@@ -184,5 +193,5 @@ def patientappointment():
 
 
 if __name__ == "__main__":
-    app.run("0.0.0.0")
+    app.run("0.0.0.0",debug=True)
 
--- Schedule/DashBoardApp/models.py
+++ Schedule/DashBoardApp/models.py
@@ -1,41 +1,41 @@
-from main import db_session
+from main import db
 from flask_login import UserMixin
 
-class Appointment(db_session.Model ,UserMixin):
+class Appointment(db.Model ,UserMixin):
     __tablename__ = 'appointments'
-    id = db_session.Column(db_session.Integer, primary_key=True)
-    start = db_session.Column(db_session.String(255))
-    end = db_session.Column(db_session.String(255))
-    all_day = db_session.Column(db_session.String(255))
-    appointment_colour = db_session.Column(db_session.String(255),nullable=False)
-    patient_id = db_session.Column(db_session.String(255))
-    appointment_type = db_session.Column(db_session.String(255))
-    canceled = db_session.Column(db_session.String(255))
+    id = db.Column(db.Integer, primary_key=True)
+    start = db.Column(db.String(255))
+    end = db.Column(db.String(255))
+    all_day = db.Column(db.String(255))
+    appointment_colour = db.Column(db.String(255),nullable=False)
+    patient_id = db.Column(db.String(255))
+    appointment_type = db.Column(db.String(255))
+    canceled = db.Column(db.String(255))
 
-class Employee(db_session.Model ,UserMixin):
+class Employee(db.Model ,UserMixin):
     __tablename__ = 'employee'
-    id = db_session.Column(db_session.Integer, primary_key=True)
-    email = db_session.Column(db_session.String(120), unique=True, nullable=False)
-    employee_id = db_session.Column(db_session.String(120), unique=True, nullable=False)
-    first_name = db_session.Column(db_session.String(255))
-    last_name = db_session.Column(db_session.String(255))
-    full_name = db_session.Column(db_session.String(255))
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    employee_id = db.Column(db.String(120), unique=True, nullable=False)
+    first_name = db.Column(db.String(255))
+    last_name = db.Column(db.String(255))
+    full_name = db.Column(db.String(255))
 
 
-class Patient(db_session.Model ,UserMixin):
+class Patient(db.Model ,UserMixin):
      __tablename__ = 'patients'
-     id = db_session.Column(db_session.Integer, primary_key=True)
-     name = db_session.Column(db_session.String(255),nullable=False)
-     email = db_session.Column(db_session.String(120), nullable=False)
-     phone_number = db_session.Column(db_session.String(255))
-     notes = db_session.Column(db_session.String(255))
+     id = db.Column(db.Integer, primary_key=True)
+     name = db.Column(db.String(255),nullable=False)
+     email = db.Column(db.String(120), nullable=False)
+     phone_number = db.Column(db.String(255))
+     notes = db.Column(db.String(255))
 
-# class Event(db_session.Model):
+# class Event(db.Model):
     
 #     __tablename__ = 'appointments'
-#     id = db_session.Column(db_session.String(100), primary_key=True)
-#     title = db_session.Column(db_session.String(80), unique=True, nullable=False)
-#     email = db_session.Column(db_session.String(120), unique=True, nullable=False)
+#     id = db.Column(db.String(100), primary_key=True)
+#     title = db.Column(db.String(80), unique=True, nullable=False)
+#     email = db.Column(db.String(120), unique=True, nullable=False)
 
 #     def __repr__(self):
 #         return '{} | {}  | {}'.format(self.id , self.title , self.email)
