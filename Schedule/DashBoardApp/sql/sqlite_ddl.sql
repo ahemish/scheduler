859dc33f7e3092d668f65434798c27b9f95e5a02
arran hemish <ahemish@hotmail.com>
2019-02-01 15:30:17 +0000

    change appointment_colour

--- Schedule/DashBoardApp/sql/sqlite_ddl.sql
+++ Schedule/DashBoardApp/sql/sqlite_ddl.sql
@@ -4,7 +4,7 @@ id integer primary key AUTOINCREMENT,
 start VARCHAR(255),
 end VARCHAR(255) ,
 all_day VARCHAR(255),
-class_name VARCHAR(255),
+appointment_colour VARCHAR(255),
 patient_id int,
 appointment_type VARCHAR(255),
 canceled VARCHAR(255)
